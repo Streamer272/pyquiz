4e009aaaae1bef5196594a856cffcec7401fab2f
Daniel Svitan <daniel.svitan.team7274dev@gmail.com>
2022-07-12 15:45:20 +0200

    ✨️ Adds question type

--- src/question.py
+++ src/question.py
@@ -1,13 +1,30 @@
 from dataclasses import dataclass
+from typing import List, Union
+from enum import Enum
+
+
+class QuestionType(Enum):
+    yes_no = 1
+    input_str = 2
+    input_int = 3
+    dropdown = 4
 
 
 @dataclass
 class Question:
+    question_type: QuestionType
     question: str
+    correct_answers: List[str]
     answer: str = None
+    answered_correctly: bool = False
 
 
-questions = [Question("What is your name?"), Question("What is your age?"), Question("What is your gender?")]
+questions = [
+    Question(QuestionType.input_str, "What is NRA?", ["National Rifle Association"]),
+    Question(QuestionType.input_str, "Who is currect president of the United States of America?", ["Joe Biden"]),
+    Question(QuestionType.input_str, "What is Germanies most known band?", ["Rammstein"]),
+    Question(QuestionType.yes_no, "Does Slovakia have 5 million citizens?", ["Yes"])
+]
 
 
 if __name__ == "__main__":
--- src/window.py
+++ src/window.py
@@ -16,40 +16,97 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from gi.repository import Gtk
-from .question import questions
+from .question import QuestionType, questions
 
 
 @Gtk.Template(resource_path='/com/streamer272/PyQuiz/window.ui')
 class PyquizWindow(Gtk.ApplicationWindow):
     __gtype_name__ = 'PyquizWindow'
 
-    ok_button = Gtk.Template.Child()
     current_question = 0
+    correct_answers = 0
 
-    q_label = Gtk.Template.Child()
-    q_entry = Gtk.Template.Child()
+    ok_button: Gtk.Button = Gtk.Template.Child()
+    q_label: Gtk.Label = Gtk.Template.Child()
+
+    q_yes_no_button: Gtk.CheckButton = Gtk.Template.Child()
+    q_input_str_entry: Gtk.Entry = Gtk.Template.Child()
+    q_input_int_entry: Gtk.Entry = Gtk.Template.Child()
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        
-        self.q_label.set_text(questions[self.current_question].question)
+
+        self.q_input_int_entry.set_input_purpose(Gtk.InputPurpose.NUMBER)
         self.ok_button.connect("clicked", self.on_ok_clicked)
+        self.render_question()
 
     def on_ok_clicked(self, _):
-        q_entry_buffer = self.q_entry.get_buffer()
-        questions[self.current_question].answer = q_entry_buffer.get_text()
-        q_entry_buffer.set_text("", -1)
-        self.q_entry.grab_focus_without_selecting()
+        self.check_question_answer()
 
         if (self.current_question + 1 >= len(questions)):
             return self.finish()
         self.current_question += 1
-        self.q_label.set_text(questions[self.current_question].question)
+        self.render_question()
+
+    def render_question(self):
+        question = questions[self.current_question]
+        self.q_label.set_text(question.question)
+
+        if question.question_type == QuestionType.yes_no:
+            self.q_yes_no_button.show()
+            self.q_input_str_entry.hide()
+            self.q_input_int_entry.hide()
+            self.q_yes_no_button.grab_focus_without_selecting()
+        elif question.question_type == QuestionType.input_str:
+            self.q_yes_no_button.hide()
+            self.q_input_str_entry.show()
+            self.q_input_int_entry.hide()
+            self.q_input_str_entry.grab_focus_without_selecting()
+        elif question.question_type == QuestionType.input_int:
+            self.q_yes_no_button.hide()
+            self.q_input_str_entry.hide()
+            self.q_input_int_entry.show()
+            self.q_input_int_entry.grab_focus_without_selecting()
+
+    def check_question_answer(self):
+        question = questions[self.current_question]
+
+        if question.question_type == QuestionType.yes_no:
+            question = questions[self.current_question]
+            question.answer = "Yes" if self.q_yes_no_button.get_active() else "No"
+            self.q_yes_no_button.set_active(False)
+
+            if question.answer in question.correct_answers:
+                question.answered_correctly = True
+                self.correct_answers += 1
+
+        elif question.question_type == QuestionType.input_str:
+            question = questions[self.current_question]
+            q_entry_buffer = self.q_input_str_entry.get_buffer()
+            question.answer = q_entry_buffer.get_text()
+            q_entry_buffer.set_text("", -1)
+
+            if question.answer.lower() in [str(i).lower() for i in question.correct_answers]:
+                question.answered_correctly = True
+                self.correct_answers += 1
+
+        elif question.question_type == QuestionType.input_int:
+            question = questions[self.current_question]
+            q_entry_buffer = self.q_input_int_entry.get_buffer()
+            question.answer = str(q_entry_buffer.get_text())
+            q_entry_buffer.set_text("", -1)
+
+            if int(question.answer) in question.correct_answers:
+                question.answered_correctly = True
+                self.correct_answers += 1
 
     def finish(self):
-        self.q_label.set_text("Thank you for completing the survey!")
+        self.q_label.set_text(f"You got {self.correct_answers} {'answer' if self.correct_answers == 1 else 'answers'} correctly")
         self.ok_button.hide()
-        self.q_entry.hide()
+
+        self.q_yes_no_button.hide()
+        self.q_input_str_entry.hide()
+        self.q_input_int_entry.hide()
 
 
 class AboutDialog(Gtk.AboutDialog):
